12ecbcb4ee4c38c3751c704a0a375790870c2be7
yuri0726 <76725736+yuri0726@users.noreply.github.com>
2021-04-01 17:53:38 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -28,3 +28,36 @@ function changeBg() {
 };
 
 var changeHero = setInterval(changeBg, 5000);
+
+// FAQ Accordion
+
+   // Accordion 메뉴 오픈 상태 변수
+   var faqOpenState = [];
+   var faqLi = document.getElementsByClassName('faq_li');
+   var faqBtn = document.getElementsByClassName('faq_q_btn');
+
+   for (i=0; i<faqLi.length; i++) {
+      faqOpenState[i] = false;
+   }
+
+   function accOpen(target) {
+
+      for (i=0; i<faqLi.length; i++) {
+
+         if (target != i) {
+            faqLi[i].style = "max-height:60px;";
+            faqBtn[i].style = "transform:rotate(0deg);";
+         faqOpenState[i] = false
+         }
+      }
+
+      if (faqOpenState[target] == true) {
+         faqLi[target].style = "max-height:60px;";
+         faqBtn[target].style = "transform:rotate(0deg);";
+         faqOpenState[target] = false;
+      } else {
+         faqLi[target].style = "max-height:600px;";
+         faqBtn[target].style = "transform:rotate(45deg);";
+         faqOpenState[target] = true;
+      };
+   }
